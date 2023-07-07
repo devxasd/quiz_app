ebf257d113a083e5cf38e8730ff124a24a7aa39b
Asad Ullah <devxasd@gmail.com>
2023-07-07 12:27:51 +0500

    Lecture 18 Reusable, Custom Styled Button

---
+++ lib/answer_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.answerText,
+    required this.onTap,
+  });
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -9,15 +10,27 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          OutlinedButton(
-            onPressed: () {},
-            child: const Text('Go Back'),
-          ),
-        ],
+    return SizedBox(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Question',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            AnswerButton(answerText: 'Answer 1', onTap: () {}),
+            AnswerButton(answerText: 'Answer 2', onTap: () {}),
+            AnswerButton(answerText: 'Answer 3', onTap: () {}),
+            AnswerButton(answerText: 'Answer 4', onTap: () {}),
+          ],
+        ),
       ),
     );
   }
