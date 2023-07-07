2b7a91a72b3d96211205b5470bc22908fac479e0
Asad Ullah <devxasd@gmail.com>
2023-07-07 11:18:56 +0500

    Lecture 16 Adding a Data Model & Dummy Data

---
+++ lib/data/questions.dart
@@ -0,0 +1,22 @@
+import 'package:quiz_app/model/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion(
+    'Flutter uses which language ?',
+    [
+      'Dart',
+      'Python',
+      'Java',
+      'PHP',
+    ],
+  ),
+  QuizQuestion(
+    'NetBeans uses which language ?',
+    [
+      'Java',
+      'Python',
+      'Dart',
+      'PHP',
+    ],
+  ),
+];
---
+++ lib/model/quiz_questions.dart
@@ -0,0 +1,5 @@
+class QuizQuestion {
+  const QuizQuestion(this.question, this.choices);
+  final String question;
+  final List<String> choices;
+}
