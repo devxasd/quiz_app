e66bef99af427f1562b446891c21142d620aff0b
Asad Ullah <devxasd@gmail.com>
2023-07-07 08:28:02 +0500

    Lecture 1 Intro

---
+++ lib/start_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class StartScreen extends StatelessWidget{
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context){
+    return const Scaffold(
+      body: Text("Quiz App"),
+    );
+  }
+}
