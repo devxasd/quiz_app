f98f268f978831266f5f405fcf0aa58a71506a8f
Asad Ullah <devxasd@gmail.com>
2023-07-08 08:25:02 +0500

    Quiz App v1.0

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -13,10 +13,37 @@ const questions = [
   QuizQuestion(
     'NetBeans uses which language ?',
     [
-      'Java 1',
-      'Python 1',
-      'Dart 1',
-      'PHP 1',
+      'Java',
+      'Python',
+      'Dart',
+      'PHP',
+    ],
+  ),
+  QuizQuestion(
+    'Which widget should you try to use more often: StatelessWidget or StatefulWidget?',
+    [
+      'StatelessWidget',
+      'StatefulWidget',
+      'Both are equally good',
+      'None of the above',
+    ],
+  ),
+  QuizQuestion(
+    'What happens if you change data in a StatelessWidget?',
+    [
+      'The UI is not updated',
+      'The UI is updated',
+      'The closest StatefulWidget is updated',
+      'Any nested StatefulWidgets are updated',
+    ],
+  ),
+  QuizQuestion(
+    'How should you update data inside of StatefulWidgets?',
+    [
+      'By calling setState()',
+      'By calling updateData()',
+      'By calling updateUI()',
+      'By calling updateState()',
     ],
   ),
 ];
--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -8,29 +8,66 @@ class QuestionsSummary extends StatelessWidget {
   @override
   Widget build(context) {
     return SizedBox(
-      height: 50,
-      child: SingleChildScrollView(
-        child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            children: questionsSummary.map(
-              (data) {
-                return Row(children: [
-                  Text(
-                    ((data['question-index'] as int) + 1).toString(),
-                  ),
-                  Expanded(
-                    child: Column(
+      child: Container(
+        height: 200,
+        padding: const EdgeInsets.all(15),
+        decoration: const BoxDecoration(
+          color: Colors.black12,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 40),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: 400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: questionsSummary.map(
+                (data) {
+                  return Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(data['question'].toString()),
-                        Text(data['correct-answer'].toString()),
-                        Text(data['user-answer'].toString()),
-                      ],
-                    ),
-                  )
-                ]);
-              },
-            ).toList(),
+                        Container(
+                          margin: const EdgeInsets.only(right: 20),
+                          width: 30,
+                          height: 30,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.amber,
+                          ),
+                          child: Text(
+                            ((data['question-index'] as int) + 1).toString(),
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(data['question'].toString()),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    data['user-answer'].toString(),
+                                    style: const TextStyle(
+                                      color: Colors.blueAccent,
+                                    ),
+                                  ),
+                                  Text(
+                                    '(${data['correct-answer']})',
+                                    style: const TextStyle(
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ]);
+                },
+              ).toList(),
+            ),
           ),
         ),
       ),
